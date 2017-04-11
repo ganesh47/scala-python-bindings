679ba286254ee3a79a1c3ef6e143103e20f2907b
ganesh@thedatateam.in <ganesh@thedatateam.in>
2017-04-12 03:02:40 +0530

    adding jep dist pack paths too!

--- src/test/scala/EnvIndependentUtils.scala
+++ src/test/scala/EnvIndependentUtils.scala
@@ -13,6 +13,7 @@ object EnvIndependentUtils {
     val os = System.getProperty("os.name").split(" ")(0).toLowerCase
     if (!os.startsWith("win")) {
       unsafe.Utils.unsafeAddDir(new File("/usr/lib/x86_64-linux-gnu/").getAbsolutePath)
+      unsafe.Utils.unsafeAddDir(new File("/usr/local/lib/python3.4/dist-packages/jep/").getAbsolutePath)
     }
     System.load(new File("jep_deps/lib/").getAbsolutePath + (if (os.startsWith("win")) "/jep.dll" else "/libjep.so"))
     instance=new Jep()
