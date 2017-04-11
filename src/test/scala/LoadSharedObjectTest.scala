ca0162660cdf79c604b38b2ef42b58a141b92d98
ganesh@thedatateam.in <ganesh@thedatateam.in>
2017-04-11 18:59:55 +0530

    ensures test checks for SO in java.lib.path

--- src/test/scala/LoadSharedObjectTest.scala
+++ src/test/scala/LoadSharedObjectTest.scala
@@ -1,12 +1,22 @@
+import java.io.File
+
 import org.scalatest.{FlatSpec, Matchers}
 
 /**
   * Created by Ganesh on 4/11/2017.
   */
-class LoadSharedObjectTest extends FlatSpec with Matchers{
+class LoadSharedObjectTest extends FlatSpec with Matchers {
 
-  "Load shared object check for jvm" should "work otherwise something is wrong in setup" in{
-
-    println(System.getProperty("java.library.path"))
+  "Load shared object check for jvm" should "work otherwise something is wrong in setup" in {0
+    val paths = System.getProperty("java.library.path").replace('[', ' ').replace(']', ' ').split("(,|;)", -1)
+    assert(paths.map(x => x.replace('\'', ' ')).exists {
+      x =>
+        val file = new File(x)
+        println(file.exists()+" for "+file.getAbsolutePath+" is dir "+file.isDirectory)
+        if(file.exists() && file.isDirectory())
+          file.list().exists(_.matches("(lib)*jep.(so|dll)"))
+        else
+          false
+    })
   }
 }
