e0d3188120a3f851c44bd71a821b44e2cb860edf
ganesh@thedatateam.in <ganesh@thedatateam.in>
2017-04-12 02:21:14 +0530

    changing caching logic!

--- src/test/scala/EnvIndependentUtils.scala
+++ src/test/scala/EnvIndependentUtils.scala
@@ -6,14 +6,17 @@ import jep.Jep
   * Created by Ganesh on 4/12/2017.
   */
 object EnvIndependentUtils {
-  private val instance:Jep=new Jep()
+  private var instance:Jep=null
   def setup :Jep= {
+    if(instance==null){
     unsafe.Utils.unsafeAddDir(new File("jep_deps/lib/").getAbsolutePath)
     val os = System.getProperty("os.name").split(" ")(0).toLowerCase
     if (!os.startsWith("win")) {
       unsafe.Utils.unsafeAddDir(new File("/usr/lib/x86_64-linux-gnu/").getAbsolutePath)
     }
     System.load(new File("jep_deps/lib/").getAbsolutePath + (if (os.startsWith("win")) "/jep.dll" else "/libjep.so"))
+    instance=new Jep()
+    }
     return instance
   }
 }
