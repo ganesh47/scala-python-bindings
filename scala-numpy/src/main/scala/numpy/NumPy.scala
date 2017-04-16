34b8762a19fd790d1cfa2ec4489aa1350427b466
ganesh@thedatateam.in <ganesh@thedatateam.in>
2017-04-17 03:49:09 +0530

    addding more tests

--- scala-numpy/src/main/scala/numpy/NumPy.scala
+++ scala-numpy/src/main/scala/numpy/NumPy.scala
@@ -8,7 +8,18 @@ class NumPy(o: Object)(implicit jep: Jep) extends ObjectFascade(o) {
     dynamic.asarray(s).as[NDArray[T]]
   }
 
-  def array[T](s:Seq[T])(implicit writer: ObjectWriter[T], reader: ObjectReader[T]): NDArray[T] = native
+  def shape[T](s: NDArray[T])(implicit writer: ObjectWriter[T], reader: ObjectReader[T]): Seq[Int] = native
+
+  def array[T](s: Seq[T])(implicit writer: ObjectWriter[T], reader: ObjectReader[T]): NDArray[T] = native
+
+  def dot[T](a: NDArray[T], b: NDArray[T])(implicit writer: ObjectWriter[T], reader: ObjectReader[T]): NDArray[T] = native
+
+  def vdot[T](a: NDArray[T], b: NDArray[T])(implicit writer: ObjectWriter[T], reader: ObjectReader[T]) = dynamic.vdot(a, b)
+
+  def inner[T](a: NDArray[T], b: NDArray[T])(implicit writer: ObjectWriter[T], reader: ObjectReader[T]) :NDArray[T] = native
+  def outer[T](a: NDArray[T], b: NDArray[T])(implicit writer: ObjectWriter[T], reader: ObjectReader[T]) :NDArray[T] = native
+  def matmul[T](a: NDArray[T], b: NDArray[T])(implicit writer: ObjectWriter[T], reader: ObjectReader[T]) :NDArray[T] = native
+
 
   def resize[T](s: Seq[T], shape: (Int, Int))(implicit writer: ObjectWriter[T], reader: ObjectReader[T]): NDArray[NDArray[Double]] = {
     dynamic.resize(s, shape).as[NDArray[NDArray[Double]]]
@@ -19,6 +30,7 @@ class NumPy(o: Object)(implicit jep: Jep) extends ObjectFascade(o) {
   }
 
   def random: NumPyRandom = dynamic.random.as[NumPyRandom]
+
   def nan: Object = dynamic.nan.as[Object]
 
   def float32: NumPyType = dynamic.float32.as[NumPyType]
--- src/test/scala/numpytest/NumpyNDArrayTests.scala
+++ src/test/scala/numpytest/NumpyNDArrayTests.scala
@@ -6,21 +6,32 @@ import numpy.NumPy
 import org.scalatest.{FlatSpec, Matchers}
 import py.py
 import utils.EnvIndependentUtils._
+
 /**
   * Created by Ganesh on 4/14/2017.
   */
-class NumpyNDArrayTests extends FlatSpec with Matchers{
+class NumpyNDArrayTests extends FlatSpec with Matchers {
 
-  "NDArrayTest" should " work " in{
+  "NDArrayTest" should " work " in {
     println(ManagementFactory.getRuntimeMXBean().getName().split("@")(0))
     println("shuhu")
-    implicit val jep=setup
+    implicit val jep = setup
     val sys = py.module("sys")
     sys.argv = Array("jep")
     sys.path.insert(0, "./src/main/python")
     println(sys.version)
-    val np=py.module("numpy").as[NumPy]
-    println(np.random.rand(100).astype(np.float32))
+    val np = py.module("numpy").as[NumPy]
+    val matOne = Seq(Seq(1.2, 2.3), Seq(1.1, 2.2))
+    val matTwo = Seq(Seq(1.2, 2.3), Seq(1.1, 2.2))
+    val first = np.array(matOne)
+    val second = np.array(matTwo)
+    assert(first.toList.containsSlice(matOne))
+    assert(np.shape(first).containsSlice(Seq(2, 2)))
+    assert(np.shape(np.dot(first, second)).containsSlice(Seq(2,2)))
+    assert(java.lang.Double.valueOf(np.vdot(first, second).toString).compareTo(12.78d) == 0)
+    assert(np.shape(np.inner(first, second)).containsSlice(Seq(2,2)))
+    assert(np.shape(np.matmul(first, second)).containsSlice(Seq(2,2)))
+    assert(np.shape(np.outer(first, second)).containsSlice(Seq(4,4)))
 
 
   }
